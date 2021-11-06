0a1001540e56009eeb1269ef186f4704f2d82520
prasanth752 <prasanth752 c0796752@mylambton.ca>
2021-11-06 10:50:12 -0400

    components-css-grouping

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/home/Home';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import './App.css';
 
 export const App = () => {
