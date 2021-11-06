552f31eed2fc17aabd03d653920c106e3feb5e89
prasanth752 <prasanth752 c0796752@mylambton.ca>
2021-11-05 23:09:01 -0400

    changes

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ const comicsRouter = require('./routes/comics')
 app.use('/comics', comicsRouter)
 
 if (process.env.NODE_ENV !== 'production') {
-    // app.use(express.static('client/build'));
+    // app.use(express.static('client/build')); 
+
     app.use(express.static(path.resolve(__dirname, "./client/build")));
 }
 
