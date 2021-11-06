7941123349adfe2d2f04b39ff9eb1918a0f4144a
prasanth752 <prasanth752 c0796752@mylambton.ca>
2021-11-06 09:47:28 -0400

    changes

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout(props) {
                     {props.children}
                 </div>
             </div>
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 };
